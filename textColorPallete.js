ca911a38af601e1a89be9c26e21363751aa62ffc
= <=>
2023-01-21 22:27:32 +0530

    Added

---
+++ textColorPallete.js
@@ -0,0 +1,35 @@
+function cl1(){
+    let inputTextArea = document.getElementById('textArea');
+    inputTextArea.style.color = "red";
+}
+
+function cl2(){
+    let inputTextArea = document.getElementById('textArea');
+    inputTextArea.style.color = "green";
+}
+
+function cl3(){
+    let inputTextArea = document.getElementById('textArea');
+    inputTextArea.style.color = "blue";
+}
+
+function cl4(){
+    let inputTextArea = document.getElementById('textArea');
+    inputTextArea.style.color = "orangered";
+}
+
+function cl5(){
+    let inputTextArea = document.getElementById('textArea');
+    inputTextArea.style.color = "black";
+}
+
+function cl6(){
+    let inputTextArea = document.getElementById('textArea');
+    inputTextArea.style.color = "purple";
+}
+
+function cl7(){
+    let inputTextArea = document.getElementById('textArea');
+    inputTextArea.style.color = "grey";
+}
+
